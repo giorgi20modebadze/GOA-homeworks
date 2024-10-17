cd0944d769276c5aeed6e010280fa4e089db13d6
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2024-10-17 20:39:02 +0400

    second commit

---
+++ level03/homework/hm2.py
@@ -0,0 +1,12 @@
+# შექმენით რამდენიმე ცვლადი სადაც შეინახავთ თქვენს შესახებ ინფორმაციას
+
+user_name =  "giorgi"
+user_surname = "modebadze"
+age = "20"
+height = "188.5"
+favourite_sport = "football"
+address = "tbilisi"
+
+# გამოიტანეატ ტერმინალში თქვენს შესახებ იფნორმაცის ცვლადების დახმარებით ერთ გრძელ წინადადებად
+
+print(user_name + " " + user_surname + " " + age + " " + height + " " + favourite_sport + " " + address )
---
+++ level04/homework/hm5, 6.py
@@ -0,0 +1,13 @@
+#5) მოახდინეთ კონკატენაცია ორი სტრინგ ტიპის ცვლადის ერთში იყოს თქვემი სახელი მეორეში გვარი
+
+user_name = "giorgi"
+user_surname = "modebadze"
+
+print(user_name + user_surname)
+
+
+#6) გაამრავლეთ  5-ზე price ცვლადი რომლის მნიშვნელობაც იქნება 100
+
+price = 100
+
+print(price * 5)
