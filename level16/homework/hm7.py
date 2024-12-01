29181c41f0c9718c402f5097d06dd6940c545c90
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2024-12-01 20:48:20 +0400

    sixteenth commit

---
+++ level16/homework/hm7.py
@@ -0,0 +1,9 @@
+#დედამ გაგიშვათ მაღაზიაში რადგან გენახათ მათ ჰქონდათ თუ არა მინერალური წყალი. თუ მაღაზიას აქვს მინერალური წყალი, დედას უთხარით რომ მაღაზიაში მინერალური წყალი აქვთ, თუ არ უდევთ დახლებზე მაშინ უთხარით რომ აღარ აქვთ. 
+
+mineraluri_wyali = input("aqvt mineraluri wyali?: ")
+
+if mineraluri_wyali == "True":
+    print("aqvt mineraluri wyali magaziashi")
+
+elif mineraluri_wyali == "False":
+    print("ar aqvt mineraluri wyali")
