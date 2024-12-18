671c359395cb5422b9ae820cc72208e4f151ff7b
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2024-12-18 15:05:12 +0400

    twentieth commit

---
+++ level20/homework/hm16.py
@@ -0,0 +1,11 @@
+#შეიყვანეთ რიცხვი და გამოიყენეთ while ციკლი, რომ შეამოწმოთ, არის თუ არა რიცხვი დადებითი.
+
+
+
+num = int(input("enter number: "))
+
+while num > 0:
+    print("dadebitia")
+
+while num < 0:
+    print("uaryofitia")
